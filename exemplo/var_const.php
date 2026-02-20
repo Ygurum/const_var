1ea75491f8852e3376007fb0eb3f795972f1b459
Ygor Menezes <ygrmns@gmail.com>
2026-02-20 17:09:33 -0300

    aula variáveis e constantes

---
+++ exemplo/var_const.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Const_var</title>
+</head>
+<body>
+    <?php 
+    
+    /*int = numero,
+    string = caractere,
+    booleano = true/false,
+    float = 0.0 */
+
+    $aluno = "Arnoldo xhuaseneguer";
+    $idade = "130"; 
+    
+    //const inicia com a palavra "define"
+    /*entre aspas duplas 
+    1º constante
+    2º valor*/
+    
+    define("instituicao","cetam");
+
+    echo "Olá aluno, $aluno $idade bem vindo ao " . instituicao 
+
+
+    
+    
+    
+    
+    ?>
+</body>
+</html>
