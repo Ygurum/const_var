a96b3f94bd6ad21fe2b17688c61b8f9f417839a9
Ygor Menezes <ygrmns@gmail.com>
2026-02-20 18:01:13 -0300

    Create orientacao_a_objeto_classe.php

---
+++ exemplo/orientacao_a_objeto_classe.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Classes</title>
+</head>
+<body>
+    <?php 
+    //definindo uma classe 
+    class  Carro {
+// objetos atributos
+    public $marca; 
+    public $modelo; 
+    public $ano; 
+    public $cor; 
+    public $velocidade = 0 ;
+
+    //metodos
+    public function acelerar (){
+        $this -> velocidade >=10;
+        echo "O carro acelerou. a velocidade é igual: ". $this->velocidade . "Km/h";
+    }
+
+    //segundo mmetodo
+    public function frear(){
+        if ($this->velocidade >=10)
+            $this->velocidade <=10;
+        echo "O carro acelerou. a velocidade é igual: ". $this->velocidade . "Km/h";
+
+    }
+
+
+
+
+
+
+    } 
+    
+    
+    
+    ?>
+</body>
+</html>
